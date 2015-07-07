ea653271fce3b49fb05c25a6ad719308be266262
Glassies <1328248678@qq.com>
2015-07-07 19:00:42 +0800

    2

---
+++ bcontine.java
@@ -0,0 +1,18 @@
+
+public class bcontine {
+		public static void main(String[]args){
+			int num=0;
+					for(int i=0;i>99;i++){
+				if(i%5==0){
+					System.out.println(i);
+			break;
+			
+		}
+		num++;
+		System.out.println(num);
+					}
+		}
+}
+
+
+
